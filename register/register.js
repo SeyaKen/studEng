5b910c84c849d62719cbd61467470bdf4a5f83ca
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-17 18:22:30 +0900

    I have to review vocaburalies kana wakaranai

--- register/register.js
+++ register/register.js
@@ -15,31 +15,35 @@ register.addEventListener('submit', e=>{
   console.log(register);
   const email = document.getElementById('register-email').value;
   const password = document.getElementById('register-password').value;
-  register.reset();
-  auth.createUserWithEmailAndPassword(email, password).then(cred=>{
-    return db.collection('users').doc(cred.user.uid).set({
-      Email: email,
-    }).then(() => {
-      console.log('新規登録に成功しました。');
-      // ここに指定したページに移動させる。
-      location = '../home/home.html';
+  if(password.length < 8) {
+    registerError.innerText = '7文字以上のパスワードを設定してください。';
+  } else {
+    register.reset();
+    auth.createUserWithEmailAndPassword(email, password).then(cred=>{
+      return db.collection('users').doc(cred.user.uid).set({
+        Email: email,
+      }).then(() => {
+        console.log('新規登録に成功しました。');
+        // ここに指定したページに移動させる。
+        location = '../home/home.html';
+      }).catch(err => {
+        console.log(err.message);
+        const registerError = document.getElementById('registerError');
+        registerError.innerText = err.message;
+        console.log('新規登録に失敗しました。');
+      }).catch(err => {
+        console.log(err.message);
+        const registerError = document.getElementById('registerError');
+        registerError.innerText = err.message;
+        console.log('新規登録に失敗しました。');
+      });
     }).catch(err => {
       console.log(err.message);
       const registerError = document.getElementById('registerError');
-      registerError.innerText = err.message;
-      console.log('新規登録に失敗しました。');
-    }).catch(err => {
-      console.log(err.message);
-      const registerError = document.getElementById('registerError');
-      registerError.innerText = err.message;
+      (err.message == 'The email address is already in use by another account.')
+      ? registerError.innerText = '既に登録済みです。'
+      : 
       console.log('新規登録に失敗しました。');
     });
-  }).catch(err => {
-    console.log(err.message);
-    const registerError = document.getElementById('registerError');
-    (err.message == 'The email address is already in use by another account.')
-    ? registerError.innerText = '既に登録済みです。'
-    : 
-    console.log('新規登録に失敗しました。');
-  });
+  }
 });
