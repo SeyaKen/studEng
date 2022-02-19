ef639997a1afd6eb55b68429063e7c8717534afc
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-19 16:48:02 +0900

    login email verification

--- ninshou/ninshou.js
+++ ninshou/ninshou.js
@@ -1,13 +1,14 @@
 // ユーザーがログインしているか確認
-const loginError = document.getElementById('loginError');
+auth.onAuthStateChanged(user => {
+  if(user && auth.currentUser.emailVerified) {
+    console.log('メール認証確認。');
+    location = '../home/home.html';
+  } else {
+    console.log('メール認証が終わっていません。');
+    console.log('ログインしていません。')
+  }
+});
+
 function reload(){
-  auth.onAuthStateChanged(user => {
-    if(user && auth.currentUser.emailVerified) {
-      console.log('メール認証確認。');
-      location = '../home/home.html';
-    } else {
-      console.log('メール認証確認がまだ行われていません。');
-      loginError.innerText = 'メール認証確認がまだ行われていません。'
-    }
-  });
-}
+  document.location.reload();
+};
