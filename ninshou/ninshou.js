ce15758286437eda23e30e25bb61b4d76246eea3
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-21 17:56:22 +0900

    fasfsafsadfdsfdasfeteh

--- ninshou/ninshou.js
+++ ninshou/ninshou.js
@@ -1,8 +1,19 @@
 // ユーザーがログインしているか確認
 auth.onAuthStateChanged(user => {
   if(user && auth.currentUser.emailVerified) {
-    console.log('メール認証確認。');
-    location = '../home/home.html';
+    return ds.collection('users').doc(user.uid).set({
+      name: '',
+      email: '',
+      selfIntroduction: '',
+      ProfilePicture: '',
+    }).then(() => {
+      console.log('メール認証確認。');
+      location = '../home/home.html';
+    }).catch(err => {
+      console.log(err);
+      console.log('メール認証が終わっていません。');
+      console.log('ログインしていません。');
+  });
   } else {
     console.log('メール認証が終わっていません。');
     console.log('ログインしていません。')
