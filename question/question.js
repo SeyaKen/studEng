290f3072ff2ba8b34b7cf078cd6e50ee6db4eacd
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-02-24 16:07:51 +0900

    hoooooooooooooooooooooooooooooo

--- question/question.js
+++ question/question.js
@@ -53,6 +53,42 @@ auth.onAuthStateChanged(userr => {
 var user;
 var uid;
 
+// +ボタンの処理
+// 監視ターゲットの取得
+const target = document.getElementById('question-container-inner1');
+const target1 = document.getElementById('plus-buttton-container');
+
+// オブザーバーの作成
+const observer = new MutationObserver(records => {
+  console.log(target.lastElementChild);
+  let child = target.lastElementChild;
+
+  // 要素の位置座標を取得
+  var clientRect = child.getBoundingClientRect();
+  // 画面の左端から、要素の左端までの距離
+  var x = clientRect.left - 45;
+  // 画面の上端から、要素の上端までの距離
+  var y = clientRect.top -3;
+
+  let plusButton = document.createElement('button');
+  plusButton.className = 'plus-button';
+  let plusIcon = document.createElement('i');
+  plusIcon.className = 'fas fa-plus fa-lg fa-fw plus-button-icon';
+  plusButton.appendChild(plusIcon);
+  plusButton.setAttribute('onclick', '');
+  target1.appendChild(plusButton);
+  console.log(y);
+  target1.style.display = "block";
+  target1.style.top = `${y}px`;
+  target1.style.left = `${x}px`;
+  target1.appendChild(target1);
+});
+
+// 監視の開始
+observer.observe(target, {
+  childList: true
+});
+
 function moveToProfile() {
   location = '../profile/profile.html';
 };
