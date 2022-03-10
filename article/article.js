e0b51d1550908f3c52dcf3cd000313a5db6f1f3e
SeyaKen <75517657+SeyaKen@users.noreply.github.com>
2022-03-10 16:56:36 +0900

    I have to review my vocaburaries and I want to go to the bathroom

--- article/article.js
+++ article/article.js
@@ -99,6 +99,149 @@ function renderData(individualDoc) {
   }
 };
 
+// +ボタンの処理
+// 監視ターゲットの取得
+const target = document.getElementById('answer-container-inner1');
+const target1 = document.getElementById('plus-buttton-container');
+
+// keyが押し込まれた時に発火する関数
+// 一番上の行の時だけbackspace押すと、サブタイトルになる関数
+var even;
+document.addEventListener('keydown', (event) => {
+  even = event;
+  // 一番下の方に来た時自動で少し
+  // 要素の位置座標を取得
+  if(event.keyCode == 13) {
+    let child = target.lastElementChild;
+    var clientRect = child.getBoundingClientRect();
+    var A = document.documentElement;
+    var Y = A.scrollHeight - A.clientHeight;
+    if(clientRect.top > 200) {
+      window.scroll({top: Y - 200, left: 0, behavior: 'smooth'});
+    }
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  if(target.innerHTML == ''
+  && event.code == 'Backspace') {
+    let pTag = document.createElement('p');
+    let brTag = document.createElement('br');
+    pTag.appendChild(brTag);
+    target.appendChild(pTag);
+  }
+});
+
+// figureの監視
+const observer = new MutationObserver(records => {
+  try{
+    if(records[0].addedNodes[0].nodeName == 'FIGURE'
+    && records[0].addedNodes[0].innerHTML == '<br>') {
+      kesitai = records[0].addedNodes[0].setAttribute('id', 'kesitai');
+    }
+  } catch(e) {
+    console.log(e);
+  }
+});
+  
+  // 監視の開始
+  observer.observe(target, {
+    childList: true
+  });
+
+// buttonを使ってinputを押す処理
+function inputClick() {
+  document.getElementById('files').click();
+}
+
+// Firebaseに画像をアップロードする関数
+function uploadData() {
+  var randomStrings1 = Math.random().toString(32).substring(2);
+  var randomStrings2 = Math.random().toString(32).substring(2);
+  var randomStrings = randomStrings1 + randomStrings2;
+  let file = document.getElementById('files').files[0];
+  console.log(file);
+  let thisRef = storagePersonalRef.child(randomStrings);
+  thisRef.put(file).then(res=> {
+    thisRef.getDownloadURL().then(url => {
+        let pTag = document.createElement('p');
+        let brTag = document.createElement('br');
+        let buttonTag = document.createElement('button');
+        let xTag = document.createElement('p');
+        xTag.innerHTML = '×'; 
+        buttonTag.setAttribute('value', randomStrings);
+        buttonTag.setAttribute('onclick', 'deletePicture(this.value)');
+        buttonTag.setAttribute('contenteditable', 'false');
+        buttonTag.setAttribute('class', 'delete-picture-button');
+        buttonTag.appendChild(xTag);
+        pTag.appendChild(brTag);
+        let figureParent = document.createElement('figure');
+        figureParent.setAttribute('class', 'question-picture0');
+        figureParent.setAttribute('id', randomStrings);
+        figureParent.setAttribute('value', url);
+        let childImg = document.createElement('img');
+        childImg.setAttribute('src', url);
+        childImg.setAttribute('width', '100%');
+        childImg.setAttribute('contenteditable', 'false');
+        childImg.setAttribute('draggabel', 'false');
+        figureParent.appendChild(buttonTag);
+        figureParent.appendChild(childImg);
+        target.appendChild(figureParent);
+        target.appendChild(pTag);
+    }).catch(e => {
+      console.log(e);
+    });
+  }).catch(e => {
+    console.log('エラー' + e);
+  });
+}
+
+function deletePicture(value) {
+  console.log(value);
+  document.getElementById(value).remove();
+  storagePersonalRef.child(value).delete();
+}
+
+function focusDetect() {
+  document.getElementById('placeholder').classList.remove("before-click");
+  document.getElementById('placeholder').classList.add("after-click");
+  document.getElementById('placeholder').innerHTML = '<br>';
+  document.getElementById('placeholder').removeAttribute('onclick');
+}
+
+// データベースに情報を入れる関数
+function questionInsert() {
+  const questionList = [];
+  let children = document.getElementById('question-container-inner1').children;
+  for(let i = 0; i < children.length; i++) {
+    if(children[i].tagName == 'P') {
+      questionList.push(children[i].innerHTML);
+    } else if (children[i].tagName == 'FIGURE') {
+      questionList.push(children[i].getAttribute('value'));
+    };
+  }
+  var randomStrings1 = Math.random().toString(32).substring(2);
+  var randomStrings2 = Math.random().toString(32).substring(2);
+  var randomStrings0 = randomStrings1 + randomStrings2;
+  var date = new Date();
+  db.collection('questions').doc(randomStrings0).set({
+    caption: caption,
+    subCaption: subCaption,
+    date: date,
+    asker: uid,
+    quesitionList: questionList,
+    askerImg: imageSrc,
+    askerName: userName,
+  }).then(() => {
+    console.log('成功');
+    // ここに指定したページに移動させる。
+    location = '../home/home.html';
+  }).catch(err => {
+    console.log(err.message);
+    console.log('失敗');
+  });
+}
+
 function moveToHome() {
   location = '../home/home.html';
 };
